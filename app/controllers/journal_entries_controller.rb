9158d947dbe7fbba9eb0aaf1b3bbd94b237f7e85
mkoenke <mkoenke@sas.upenn.edu>
2021-01-22 13:48:01 -0500

    change api and database

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -20,27 +20,6 @@ class JournalEntriesController < ApplicationController
 
     def create
         journal_entry = JournalEntry.create!(journal_entry_params)
-                # create helper function to send journal entry content to API and get response of emotion data
-                # use emotion data to create Report.new and send to front end to display charts
-
-                # response = RestClient.post "https://apis.paralleldots.com/v5/emotion", { api_key: ENV['parallel_api_key'], text: journal_entry}
-                # response = JSON.parse( response )
-                # puts response
-                # new_report = response.each do |emotion|
-                #     Report.create(
-                        # title: journal_entry.title,
-                        # journal_entry_id: journal_entry.id,
-                        # parent_id: journal_entry.child.parent.id,
-                        # child_id: journal_entry.child.id,
-                        # excited: response["emotion"]["Excited"],
-                        # fear: response["emotion"]["Fear"],
-                        # angry: response["emotion"]["Angry"],
-                        # happy: response["emotion"]["Happy"],
-                        # sad: response["emotion"]["Sad"],
-                        # bored: response["emotion"]["Bored"],
-                #     )
-                # end
-                # puts new_report
 
         url = URI("https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/")
 
@@ -50,55 +29,47 @@ class JournalEntriesController < ApplicationController
 
         request = Net::HTTP::Post.new(url)
         request["content-type"] = 'application/x-www-form-urlencoded'
-        request["x-rapidapi-key"] = 'b5e53cdd43msh1789007757d5e6cp16375ejsn5b00f98de59e'
-        # request["x-rapidapi-key"] = '9562b59c0emsh93141dff1893ca0p1dce1cjsnc3699e6882d9'
+        request["x-rapidapi-key"] = ENV['rapidApi_key']
         request["x-rapidapi-host"] = 'twinword-emotion-analysis-v1.p.rapidapi.com'
-        # request.body = "text=After%20living%20abroad%20for%20such%20a%20long%20time%2C%20seeing%20my%20family%20was%20the%20best%20present%20I%20could%20have%20ever%20wished%20for."
         request.body = "text=#{journal_entry.content.split(" ").join("%20")}"
 
         response = http.request(request)
-        # puts response.read_body
         resp = response.read_body
-        # puts resp
-        
-        eval_resp = eval(resp)
-        puts eval_resp
-        
-       report_1 = Report.create(
-            title: journal_entry.title,
-            journal_entry_id: journal_entry.id,
-            parent_id: journal_entry.child.parent.id,
-            child_id: journal_entry.child.id,
-            anger: eval_resp.dig(:emotion_scores, :anger),
-            disgust: eval_resp.dig(:emotion_scores, :disgust),
-            fear: eval_resp.dig(:emotion_scores, :fear),
-            joy: eval_resp.dig(:emotion_scores, :joy),
-            sadness: eval_resp.dig(:emotion_scores, :sadness),
-            surprise: eval_resp.dig(:emotion_scores, :surprise),
-            )
-            puts report_1
+    #     eval_resp = eval(resp)
+    #     # puts eval_resp      
+    #    report_1 = Report.create(
+    #         title: journal_entry.title,
+    #         journal_entry_id: journal_entry.id,
+    #         parent_id: journal_entry.child.parent.id,
+    #         child_id: journal_entry.child.id,
+    #         anger: eval_resp.dig(:emotion_scores, :anger),
+    #         disgust: eval_resp.dig(:emotion_scores, :disgust),
+    #         fear: eval_resp.dig(:emotion_scores, :fear),
+    #         joy: eval_resp.dig(:emotion_scores, :joy),
+    #         sadness: eval_resp.dig(:emotion_scores, :sadness),
+    #         surprise: eval_resp.dig(:emotion_scores, :surprise),
+    #         )
+    #     puts report_1
             
-            parsed_resp = JSON.parse resp.gsub('=>', ':')
-            puts parsed_resp
+        parsed_resp = JSON.parse resp.gsub('=>', ':')
+        puts parsed_resp
 
-            inner_hash = parsed_resp["emotion_scores"]
-            puts inner_hash
+        inner_hash = parsed_resp["emotion_scores"]
+        puts inner_hash
 
-            report_2 = Report.create(
-            title: journal_entry.title,
-            journal_entry_id: journal_entry.id,
-            parent_id: journal_entry.child.parent.id,
-            child_id: journal_entry.child.id,
-            anger: inner_hash["anger"],
-            disgust: inner_hash["disgust"],
-            fear: inner_hash["fear"],
-            joy: inner_hash["joy"],
-            sadness: inner_hash["sadness"],
-            surprise: inner_hash["surprise"],
-            )
-            puts report_2
-         
-
+        report_2 = Report.create(
+        title: journal_entry.title,
+        journal_entry_id: journal_entry.id,
+        parent_id: journal_entry.child.parent.id,
+        child_id: journal_entry.child.id,
+        anger: inner_hash["anger"],
+        disgust: inner_hash["disgust"],
+        fear: inner_hash["fear"],
+        joy: inner_hash["joy"],
+        sadness: inner_hash["sadness"],
+        surprise: inner_hash["surprise"],
+        )
+        puts report_2
         render json: journal_entry
     end
     
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,8 +22,8 @@ puts "Parent seeded"
 child = Child.create!(username: "Angelina", password_digest: BCrypt::Password.create("1234"), image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1YM53mG10H_U25iPjop83QHaEo%26pid%3DApi&f=1", parent_id: parent.id)
 puts "Child seeded"
 
-journal_entry = JournalEntry.create!(title: "Day 1", child_id: child.id, content: "Today was a good day")
-puts "Journal seeded"
+# journal_entry = JournalEntry.create!(title: "Day 1", child_id: child.id, content: "Today was a good day")
+# puts "Journal seeded"
 
 # report = Report.create!(title: "Day 1", journal_entry_id: journal_entry.id, parent_id: parent.id)
 # puts "Report seeded"
