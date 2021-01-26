7132f417451f92d62723c95060d8c7ea36883ef7
mkoenke <mkoenke@sas.upenn.edu>
2021-01-26 11:01:32 -0500

    comment out post to API

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -20,58 +20,51 @@ class JournalEntriesController < ApplicationController
 
     def create
         journal_entry = JournalEntry.create!(journal_entry_params)
+        byebug
+        attached_clip = journal_entry.clip.attach(params[:clip])
+        puts attached_clip
 
-        url = URI("https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/")
+        url = Rails.application.routes.url_helpers.rails_blob_url(attached_clip, only_path: true)
+        render json: {message: "Attached to File", url: url}
 
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        # url = URI("https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/")
 
-        request = Net::HTTP::Post.new(url)
-        request["content-type"] = 'application/x-www-form-urlencoded'
-        request["x-rapidapi-key"] = ENV['rapidApi_key']
-        request["x-rapidapi-host"] = 'twinword-emotion-analysis-v1.p.rapidapi.com'
-        request.body = "text=#{journal_entry.content.split(" ").join("%20")}"
+        # http = Net::HTTP.new(url.host, url.port)
+        # http.use_ssl = true
+        # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-        response = http.request(request)
-        resp = response.read_body
-    #     eval_resp = eval(resp)
-    #     # puts eval_resp      
-    #    report_1 = Report.create(
-    #         title: journal_entry.title,
-    #         journal_entry_id: journal_entry.id,
-    #         parent_id: journal_entry.child.parent.id,
-    #         child_id: journal_entry.child.id,
-    #         anger: eval_resp.dig(:emotion_scores, :anger),
-    #         disgust: eval_resp.dig(:emotion_scores, :disgust),
-    #         fear: eval_resp.dig(:emotion_scores, :fear),
-    #         joy: eval_resp.dig(:emotion_scores, :joy),
-    #         sadness: eval_resp.dig(:emotion_scores, :sadness),
-    #         surprise: eval_resp.dig(:emotion_scores, :surprise),
-    #         )
-    #     puts report_1
-            
-        parsed_resp = JSON.parse resp.gsub('=>', ':')
-        puts parsed_resp
+        # request = Net::HTTP::Post.new(url)
+        # request["content-type"] = 'application/x-www-form-urlencoded'
+        # request["x-rapidapi-key"] = ENV['rapidApi_key']
+        # request["x-rapidapi-host"] = 'twinword-emotion-analysis-v1.p.rapidapi.com'
+        # request.body = "text=#{journal_entry.content.split(" ").join("%20")}"
 
-        inner_hash = parsed_resp["emotion_scores"]
-        puts inner_hash
+        # response = http.request(request)
+        # resp = response.read_body
+       
+        # parsed_resp = JSON.parse resp.gsub('=>', ':')
+        # puts parsed_resp
 
-        report_2 = Report.create(
-        title: journal_entry.title,
-        journal_entry_id: journal_entry.id,
-        parent_id: journal_entry.child.parent.id,
-        child_id: journal_entry.child.id,
-        anger: inner_hash["anger"],
-        disgust: inner_hash["disgust"],
-        fear: inner_hash["fear"],
-        joy: inner_hash["joy"],
-        sadness: inner_hash["sadness"],
-        surprise: inner_hash["surprise"],
-        )
-        puts report_2
+        # inner_hash = parsed_resp["emotion_scores"]
+        # puts inner_hash
+
+        # report_2 = Report.create(
+        # title: journal_entry.title,
+        # journal_entry_id: journal_entry.id,
+        # parent_id: journal_entry.child.parent.id,
+        # child_id: journal_entry.child.id,
+        # anger: inner_hash["anger"],
+        # disgust: inner_hash["disgust"],
+        # fear: inner_hash["fear"],
+        # joy: inner_hash["joy"],
+        # sadness: inner_hash["sadness"],
+        # surprise: inner_hash["surprise"],
+        # )
+        # puts report_2
         render json: journal_entry
+        puts journal_entry
     end
+
     
     def destroy
         journal_entry = JournalEntry.find(params[:id])
