a3ae418992e8615780916fcf18dc2d0218fe0f81
mkoenke <mkoenke@sas.upenn.edu>
2021-01-30 16:07:34 -0500

    add correct url for video entry

--- app/models/video_entry.rb
+++ app/models/video_entry.rb
@@ -2,12 +2,12 @@ class VideoEntry < ApplicationRecord
     include Rails.application.routes.url_helpers
     has_one_attached :video, dependent: :destroy
     belongs_to :child
-    has_one :report
+    has_one :video_report
 
 
     def url
         if self.video.attached?
-          Rails.application.routes.url_helpers.rails_blob_path(self.video, only_path: true)
+          "http://localhost:3000/#{Rails.application.routes.url_helpers.rails_blob_path(self.video, only_path: true)}"
         else
           nil
         end
